c9750f9df9d1b88b2103c64d0fef4c1968ebaba2
KevinVinsen <kvn1961@gmail.com>
2015-11-12 14:40:47 +0800

    Scripts to run the copying

---
+++ python/bash/copy_measurement_sets_000_019.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+cd /scratch/kevin/aws-chiles02/000
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/001
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/002
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/003
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/004
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/005
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/006
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/007
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/008
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/009
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/010
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/011
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/012
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/013
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/014
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/015
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/016
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/017
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/018
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/019
+bash -x copy_measurement_set.sh
---
+++ python/bash/copy_measurement_sets_020_042.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+cd /scratch/kevin/aws-chiles02/020
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/021
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/022
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/023
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/024
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/025
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/026
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/027
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/028
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/029
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/030
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/031
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/032
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/033
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/034
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/035
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/036
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/037
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/038
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/039
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/040
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/041
+bash -x copy_measurement_set.sh
+cd /scratch/kevin/aws-chiles02/042
+bash -x copy_measurement_set.sh
