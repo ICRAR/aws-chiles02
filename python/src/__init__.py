c045c1cd7b5387f8ac130926ed56e43d37e50f4c
KevinVinsen <kvn1961@gmail.com>
2016-01-18 07:57:06 +0800

    Modified to be just the support code

--- python/src/__init__.py
+++ python/src/__init__.py
@@ -19,35 +19,3 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 #    MA 02111-1307  USA
 #
-"""
-Get the MD5 of a file
-"""
-import argparse
-import hashlib
-import logging
-
-from os.path import exists
-
-LOG = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, format='%(asctime)-15s:' + logging.BASIC_FORMAT)
-
-
-def md5sum(filename, blocksize=8388608):
-    hash_md5 = hashlib.md5()
-    with open(filename, "rb") as f:
-        for block in iter(lambda: f.read(blocksize), b""):
-            hash_md5.update(block)
-    return hash_md5.hexdigest()
-
-
-def main():
-    parser = argparse.ArgumentParser('Get the MD5 of a file')
-    parser.add_argument('file', help='the file')
-    args = parser.parse_args()
-
-    if exists(args.file):
-        LOG.info('md5: {0}'.format(md5sum(args.file)))
-
-if __name__ == "__main__":
-    main()
-
