318ada519e92c7e2d01e4bc4cc1791d45dc24aaf
KevinVinsen <kvn1961@gmail.com>
2016-01-22 07:45:52 +0800

    Ready to try properly

--- pipeline/docker/java-s3-copy/scripts/copy_from_s3_mt.sh
+++ pipeline/docker/java-s3-copy/scripts/copy_from_s3_mt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xv
 # copy_from_s3
 
-java -classpath /chiles02/awsChiles02.jar org.icrar.awsChiles02.copyS3.CopyFileFromS3 -aws_access_key_id $3 -aws_secret_access_key $4 $1 $2
+java -classpath /chiles02/awsChiles02.jar org.icrar.awsChiles02.copyS3.CopyFileFromS3 -aws_access_key_id $3 -aws_secret_access_key $4 $1 $2.tar
 
 # Create the directory to hold the measurement set
 mkdir -p $2
