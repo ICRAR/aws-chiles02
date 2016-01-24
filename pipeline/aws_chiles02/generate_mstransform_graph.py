bedee6af6756db4a143f2299dcd6d7663c101abb
KevinVinsen <kvn1961@gmail.com>
2016-01-25 06:55:39 +0800

    Added code to use the new client

--- pipeline/aws_chiles02/generate_mstransform_graph.py
+++ pipeline/aws_chiles02/generate_mstransform_graph.py
@@ -23,24 +23,33 @@
 Build a dictionary for the execution graph
 """
 import argparse
+import getpass
 import json
 import logging
+import os
 import uuid
+
+import time
 
 from aws_chiles02.common import get_oid, get_module_name, CONTAINER_JAVA_S3_COPY, CONTAINER_CHILES02
 from dfms.apps.dockerapp import DockerApp
-from dfms.ddap_protocol import DROPLinkType
-from dfms.drop import DirectoryContainer
-from dfms.graph_loader import addLink
+from dfms.drop import DirectoryContainer, dropdict
+from dfms.manager.client import NodeManagerClient
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)-15s:' + logging.BASIC_FORMAT)
 
 
+def get_session_id():
+    return '{0}-{1}'.format(
+            getpass.getuser() + round(time.time())
+    )
+
+
 def build_graph(args):
     drop_list = []
 
-    s3_drop = {
+    s3_drop = dropdict({
         "type": 'plain',
         "storage": 's3',
         "oid": get_oid('s3'),
@@ -49,55 +58,63 @@ def build_graph(args):
         "key": args.ms_set,
         "aws_access_key_id": args.aws_access_key_id,
         "aws_secret_access_key": args.aws_secret_access_key
-    }
-    copy_from_s3 = {
+    })
+    copy_from_s3 = dropdict({
         "type": 'app',
         "app": get_module_name(DockerApp),
         "oid": get_oid('app'),
         "uid": str(uuid.uuid4()),
         "image": CONTAINER_JAVA_S3_COPY,
-        "command": 'copy_from_s3_mt.sh %iDataURL0 /dfms_root/%o0 {0} {1}'.format(args.aws_access_key_id, args.aws_secret_access_key),
+        "command": 'copy_from_s3_mt.sh %iDataURL0 %o0 {0} {1}'.format(args.aws_access_key_id, args.aws_secret_access_key),
         "user": 'root'
-    }
-    measurement_set = {
+    })
+    oid = get_oid('dir')
+    measurement_set = dropdict({
         "type": 'container',
         "container": get_module_name(DirectoryContainer),
-        "oid": get_oid('dir'),
+        "oid": oid,
         "uid": str(uuid.uuid4()),
-        "dirname": args.volume
-    }
+        "dirname": os.path.join(args.volume, oid),
+        "check_exists": False
+    })
 
-    addLink(DROPLinkType.INPUT, copy_from_s3, s3_drop['oid'])
-    addLink(DROPLinkType.OUTPUT, copy_from_s3, measurement_set['oid'])
+    copy_from_s3.addInput(s3_drop)
+    copy_from_s3.addOutput(measurement_set)
 
     drop_list.append(s3_drop)
     drop_list.append(copy_from_s3)
     drop_list.append(measurement_set)
 
-    casa_py_drop = {
+    casa_py_drop = dropdict({
         "type": 'app',
         "app": get_module_name(DockerApp),
         "oid": get_oid('app'),
         "uid": str(uuid.uuid4()),
         "image": CONTAINER_CHILES02,
-        "command": 'mstransform.sh /dfms_root/%i0 /dfms_root/%o0 /dfms_root/%o0 980 984',
+        "command": 'mstransform.sh %i0 %o0 %o0 980 984',
         "user": 'root'
-    }
-    result = {
+    })
+    oid = get_oid('dir')
+    result = dropdict({
         "type": 'container',
         "container": get_module_name(DirectoryContainer),
-        "oid": get_oid('dir'),
+        "oid": oid,
         "uid": str(uuid.uuid4()),
-        "dirname": args.volume
-    }
+        "dirname": os.path.join(args.volume, oid),
+        "check_exists": False
+    })
 
-    addLink(DROPLinkType.INPUT, casa_py_drop, measurement_set['oid'])
-    addLink(DROPLinkType.OUTPUT, casa_py_drop, result['oid'])
+    casa_py_drop.addInput(measurement_set)
+    casa_py_drop.addOutput(result)
 
-    drop_list.append(casa_py_drop)
-    drop_list.append(result)
+    json_dump = json.dumps(drop_list, indent=2)
+    LOG.info(json_dump)
+    client = NodeManagerClient(args.host, int(args.port))
 
-    print (json.dumps(drop_list, indent=2))
+    session_id = get_session_id()
+    client.create_session(session_id)
+    client.append_graph(session_id, json_dump)
+    client.deploy_session(session_id, [s3_drop['uid']])
 
 
 def parser_arguments():
@@ -107,6 +124,8 @@ def parser_arguments():
     parser.add_argument('bucket', help='the bucket to access')
     parser.add_argument('ms_set', help='the measurement set key')
     parser.add_argument('volume', help='the directory on the host to bind to the Docker Apps')
+    parser.add_argument('host', help='the host the dfms is running on')
+    parser.add_argument('port', help='the port to bind to', default=8000)
 
     args = parser.parse_args()
     return args
