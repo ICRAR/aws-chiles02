411399e94ba7227f9b1aada34edf6b079f935eb0
KevinVinsen <kvn1961@gmail.com>
2016-01-25 11:25:10 +0800

    Moved to creating special docker apps

--- pipeline/aws_chiles02/generate_mstransform_graph.py
+++ pipeline/aws_chiles02/generate_mstransform_graph.py
@@ -23,13 +23,14 @@
 Build a dictionary for the execution graph
 """
 import argparse
+import json
 import logging
 import os
 
 from aws_chiles02.common import get_oid, get_module_name, get_uid, get_session_id, get_observation
 from aws_chiles02.docker_apps import DockerCopyFromS3, DockerCopyToS3, DockerMsTransform
 from dfms.drop import DirectoryContainer, dropdict, BarrierAppDROP
-from dfms.manager.client import NodeManagerClient
+from dfms.manager.client import NodeManagerClient, SetEncoder
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)-15s:' + logging.BASIC_FORMAT)
@@ -92,7 +93,8 @@ def build_graph(args):
             "oid": oid,
             "uid": get_uid(),
             "dirname": os.path.join(args.volume, oid),
-            "check_exists": False
+            "check_exists": False,
+            "expiryAfterUse": True
         })
 
         casa_py_drop.addInput(measurement_set)
@@ -113,6 +115,7 @@ def build_graph(args):
             "storage": 's3',
             "oid": get_oid('s3'),
             "uid": get_uid(),
+            "expiryAfterUse": True,
             "bucket": args.bucket,
             "key": '{0}_{1}/{2}'.format(
                     frequency_pairs[0],
@@ -140,23 +143,47 @@ def build_graph(args):
     for output in outputs:
         barrier_drop.addInput(output)
 
+    return drop_list, [s3_drop['uid']]
+
+
+def command_json(args):
+    drop_list, start_uids = build_graph(args)
+    LOG.info(json.dumps(drop_list, indent=2, cls=SetEncoder))
+
+
+def command_run(args):
+    drop_list, start_uids = build_graph(args)
+
     client = NodeManagerClient(args.host, int(args.port))
 
     session_id = get_session_id()
     client.create_session(session_id)
     client.append_graph(session_id, drop_list)
-    client.deploy_session(session_id, [s3_drop['uid']])
+    client.deploy_session(session_id, start_uids)
 
 
 def parser_arguments():
     parser = argparse.ArgumentParser('Build the physical graph for a day')
-    parser.add_argument('aws_access_key_id', help="the AWS aws_access_key_id to use")
-    parser.add_argument('aws_secret_access_key', help="the AWS aws_secret_access_key to use")
-    parser.add_argument('bucket', help='the bucket to access')
-    parser.add_argument('ms_set', help='the measurement set key')
-    parser.add_argument('volume', help='the directory on the host to bind to the Docker Apps')
-    parser.add_argument('host', help='the host the dfms is running on')
-    parser.add_argument('-p', '--port', help='the port to bind to', default=8000)
+
+    subparsers = parser.add_subparsers()
+
+    parser_json = subparsers.add_parser('json', help='display the json')
+    parser_json.add_argument('aws_access_key_id', help="the AWS aws_access_key_id to use")
+    parser_json.add_argument('aws_secret_access_key', help="the AWS aws_secret_access_key to use")
+    parser_json.add_argument('bucket', help='the bucket to access')
+    parser_json.add_argument('ms_set', help='the measurement set key')
+    parser_json.add_argument('volume', help='the directory on the host to bind to the Docker Apps')
+    parser_json.set_defaults(func=command_json)
+
+    parser_run = subparsers.add_parser('run', help='run and deploy')
+    parser_run.add_argument('aws_access_key_id', help="the AWS aws_access_key_id to use")
+    parser_run.add_argument('aws_secret_access_key', help="the AWS aws_secret_access_key to use")
+    parser_run.add_argument('bucket', help='the bucket to access')
+    parser_run.add_argument('ms_set', help='the measurement set key')
+    parser_run.add_argument('volume', help='the directory on the host to bind to the Docker Apps')
+    parser_run.add_argument('host', help='the host the dfms is running on')
+    parser_run.add_argument('-p', '--port', help='the port to bind to', default=8000)
+    parser_run.set_defaults(func=command_run)
 
     args = parser.parse_args()
     return args
@@ -164,4 +191,4 @@ def parser_arguments():
 
 if __name__ == '__main__':
     arguments = parser_arguments()
-    build_graph(arguments)
+    arguments.func(arguments)
