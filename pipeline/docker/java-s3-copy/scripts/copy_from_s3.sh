6efd2234909a4b87a1f5a43356a876a842e1c197
KevinVinsen <kvn1961@gmail.com>
2016-02-01 06:26:14 +0800

    Return error codes

--- pipeline/docker/java-s3-copy/scripts/copy_from_s3.sh
+++ pipeline/docker/java-s3-copy/scripts/copy_from_s3.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -xv
 # copy_from_s3
 
-java -classpath /chiles02/awsChiles02.jar org.icrar.awsChiles02.copyS3.CopyFileFromS3SingleThreaded -aws_access_key_id $3 -aws_secret_access_key $4 $1 $2.tar
+java -classpath /chiles02/awsChiles02.jar org.icrar.awsChiles02.copyS3.CopyFileFromS3 -aws_access_key_id $3 -aws_secret_access_key $4 $1 $2.tar
 
-# Create the directory to hold the measurement set
-mkdir -p $2
-tar -xvf $2.tar -C $2
-rm $2.tar
+if [ $? == 0 ] && [ -f $2.tar ] ; then
+    # Create the directory to hold the measurement set
+    mkdir -p $2
+    tar -xvf $2.tar -C $2
+    rm $2.tar
+    return $?
+fi
+
+return 1
---
+++ pipeline/docker/java-s3-copy/scripts/copy_from_s3_st.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xv
+# copy_from_s3_st
+
+java -classpath /chiles02/awsChiles02.jar org.icrar.awsChiles02.copyS3.CopyFileFromS3SingleThreaded -aws_access_key_id $3 -aws_secret_access_key $4 $1 $2.tar
+
+# Create the directory to hold the measurement set
+mkdir -p $2
+tar -xvf $2.tar -C $2
+rm $2.tar
