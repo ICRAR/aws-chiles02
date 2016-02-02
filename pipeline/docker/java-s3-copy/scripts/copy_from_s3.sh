e39258c5f3b49c3cd304adc3a0da982d2795464d
markaboulton <mark.boulton@uwa.edu.au>
2016-02-02 15:21:31 +0800

    Added additional params for using more cache

--- pipeline/docker/java-s3-copy/scripts/copy_from_s3.sh
+++ pipeline/docker/java-s3-copy/scripts/copy_from_s3.sh
@@ -8,7 +8,10 @@ if [ -d $2/${measurement_set} ] ; then
   exit 0
 fi
 
-java -classpath /chiles02/awsChiles02.jar org.icrar.awsChiles02.copyS3.CopyFileFromS3 -aws_access_key_id $3 -aws_secret_access_key $4 $1 $2.tar
+# The following will need (16 + 1) * 262144000 bytes of heap space, ie approximately 4.5G.
+# Note setting minimum as well as maximum heap results in OutOfMemory errors at times!
+# The -d64 is to make sure we are using a 64bit JVM.
+java -d64 -Xms6g -Xmx6g -classpath /chiles02/awsChiles02.jar org.icrar.awsChiles02.copyS3.CopyFileFromS3 -thread_buffer 262144000 -thread_pool 16 -aws_access_key_id $3 -aws_secret_access_key $4 $1 $2.tar
 
 if [ $? == 0 ] && [ -f $2.tar ] ; then
     # Create the directory to hold the measurement set
