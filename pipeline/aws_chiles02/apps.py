e778e84350552085870f7a21750c7616add960e4
KevinVinsen <kvn1961@gmail.com>
2016-02-11 07:45:35 +0800

    Added a shutdown app to shutdown a node

--- pipeline/aws_chiles02/apps.py
+++ pipeline/aws_chiles02/apps.py
@@ -23,11 +23,14 @@
 My Docker Apps
 """
 import logging
-
+import os
+import subprocess
+import time
 import sqlite3
 
 from dfms.apps.dockerapp import DockerApp
-from dfms.drop import BarrierAppDROP
+from dfms.drop import BarrierAppDROP, FileDROP, DirectoryContainer
+from dfms import utils
 
 LOG = logging.getLogger(__name__)
 
@@ -187,7 +190,7 @@ class InitializeSqliteApp(BarrierAppDROP):
         super(InitializeSqliteApp, self).__init__(oid, uid, **kwargs)
 
     def dataURL(self):
-        return 'InitializeSqliteApp'
+        return type(self).__name__
 
     def run(self):
         self._connection = sqlite3.connect(self.inputs[0].path)
@@ -203,3 +206,72 @@ class InitializeSqliteApp(BarrierAppDROP):
 `time`	REAL NOT NULL
 )
 ''')
+
+
+class BashShellApp(BarrierAppDROP):
+    def __init__(self, oid, uid, **kwargs):
+        self._command = None
+        self._exit_code = None
+        super(BashShellApp, self).__init__(oid, uid, **kwargs)
+
+    def initialize(self, **kwargs):
+        BarrierAppDROP.initialize(self, **kwargs)
+
+        self._command = self._getArg(kwargs, 'command', None)
+        if not self._command:
+            raise Exception('No command specified, cannot create BashShellApp')
+
+    def run(self):
+        inputs = [i.path for i in self.inputs if isinstance(i, (FileDROP, DirectoryContainer))]
+        outputs = [o.path for o in self.outputs if isinstance(o, (FileDROP, DirectoryContainer))]
+
+        # Replace any input/output placeholders that might be found in the
+        # command line by the real path of the inputs and outputs
+        cmd = self._command
+        for x, i in enumerate(inputs):
+            cmd = cmd.replace('%i{0}'.format(x), i)
+        for x, o in enumerate(outputs):
+            cmd = cmd.replace('%o{0}'.format(x), o)
+
+        if LOG.isEnabledFor(logging.DEBUG):
+            LOG.debug("Command after binding placeholder replacement is: {0}".format(cmd))
+
+        # Inputs/outputs that are not FileDROPs or DirectoryContainers can't
+        # bind their data via volumes into the docker container. Instead they
+        # communicate their dataURL via command-line replacement
+        input_data_urls = [i.dataURL for i in self.inputs if not isinstance(i, (FileDROP, DirectoryContainer))]
+        output_data_urls = [o.dataURL for o in self.outputs if not isinstance(o, (FileDROP, DirectoryContainer))]
+
+        for x, i in enumerate(input_data_urls):
+            cmd = cmd.replace("%iDataURL{0}".format(x), i)
+        for x, o in enumerate(output_data_urls):
+            cmd = cmd.replace("%oDataURL{0}".format(x), o)
+
+        if LOG.isEnabledFor(logging.DEBUG):
+            LOG.debug("Command after data URL placeholder replacement is: {0}".format(cmd))
+
+        # Wrap everything inside bash
+        cmd = '/bin/bash -c "{0}"'.format(utils.escapeQuotes(cmd, singleQuotes=False))
+
+        if LOG.isEnabledFor(logging.DEBUG):
+            LOG.debug("Command after user creation and wrapping is: {0}".format(cmd))
+
+        start = time.time()
+
+        # Wait until it finishes
+        process = subprocess.Popen(cmd, bufsize=1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=os.environ.copy())
+        stdout, stderr = process.communicate()
+        self._exit_code = process.returncode
+        end = time.time()
+        if LOG.isEnabledFor(logging.INFO):
+            LOG.info("Finished in {0:.2f} [s] with exit code {1}".format(end-start, self._exit_code))
+
+        if self._exit_code == 0 and LOG.isEnabledFor(logging.DEBUG):
+            LOG.debug("Command finished successfully, output follows.\n==STDOUT==\n{0}==STDERR==\n{1}".format(stdout, stderr))
+        elif self._exit_code != 0:
+            message = "Command didn't finish successfully (exit code {0})".format(self._exit_code)
+            LOG.error(message + ", output follows.\n==STDOUT==\n%s==STDERR==\n%s" % (stdout, stderr))
+            raise Exception(message)
+
+    def dataURL(self):
+        return type(self).__name__
---
+++ pipeline/test/shutdown_test.py
@@ -0,0 +1,107 @@
+#
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+"""
+Test shutting down a server from DFMS
+"""
+import json
+import logging
+
+import argparse
+
+from aws_chiles02.apps import BashShellApp
+from aws_chiles02.common import get_session_id, get_oid, get_uid, get_module_name
+from aws_chiles02.generate_common import AbstractBuildGraph
+from dfms.drop import dropdict
+from dfms.manager.client import SetEncoder, NodeManagerClient
+
+LOG = logging.getLogger(__name__)
+
+
+class BuildGraph(AbstractBuildGraph):
+    def __init__(self, command_line_args):
+        super(BuildGraph, self).__init__(command_line_args)
+
+    def build_graph(self):
+        start_drop = dropdict({
+            "type": 'plain',
+            "storage": 'memory',
+            "oid": get_oid('memory_in'),
+            "uid": get_uid(),
+        })
+        self._start_oids.append(start_drop['uid'])
+        self.append(start_drop)
+
+        shutdown_drop = dropdict({
+            "type": 'app',
+            "app": get_module_name(BashShellApp),
+            "oid": get_oid('app_bash_shell_app'),
+            "uid": get_uid(),
+            "command": 'sudo shutdown -h +5 "DFMS node shutting down"',
+            "user": 'root',
+            "input_error_threshold": 100,
+        })
+        shutdown_drop.addInput(start_drop)
+        self.append(shutdown_drop)
+
+
+def command_json(args):
+    graph = BuildGraph(args)
+    graph.build_graph()
+    json_dumps = json.dumps(graph.drop_list, indent=2, cls=SetEncoder)
+    LOG.info(json_dumps)
+    with open("/tmp/json_split.txt", "w") as json_file:
+        json_file.write(json_dumps)
+
+
+def command_run(args):
+    graph = BuildGraph(args)
+    graph.build_graph()
+
+    client = NodeManagerClient(args.host, args.port)
+
+    session_id = get_session_id()
+    client.create_session(session_id)
+    client.append_graph(session_id, graph.drop_list)
+    client.deploy_session(session_id, graph.start_oids)
+
+
+def parser_arguments():
+    parser = argparse.ArgumentParser('Build the shutdown physical graph')
+
+    subparsers = parser.add_subparsers()
+
+    parser_json = subparsers.add_parser('json', help='display the json')
+    parser_json.set_defaults(func=command_json)
+
+    parser_run = subparsers.add_parser('run', help='run and deploy')
+    parser_run.add_argument('host', help='the host the dfms is running on')
+    parser_run.add_argument('-p', '--port', type=int, help='the port to bind to', default=8000)
+    parser_run.set_defaults(func=command_run)
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    arguments = parser_arguments()
+    arguments.func(arguments)
