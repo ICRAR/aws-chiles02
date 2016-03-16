8d197e00f36a346d62c8065147943ac499b2a677
KevinVinsen <kvn1961@gmail.com>
2016-03-17 07:03:39 +0800

    Removed the java

--- pipeline/docker/java-s3-copy/scripts/copy_casapy_logs_to_s3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xv
-# copy_casapy_logs_to_s3
-
-cd /opt/chiles02/aws-chiles02
-git pull
-
-# infile
-python /opt/chiles02/aws-chiles02/pipeline/aws_chiles02/copy_casapy_logs_to_s3.py $@
--- pipeline/docker/java-s3-copy/scripts/copy_mstransform_to_s3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xv
-# copy_mstransform_to_s3
-
-cd /opt/chiles02/aws-chiles02
-git pull
-
-# infile, outdir, min_freq, max_freq
-python /opt/chiles02/aws-chiles02/pipeline/aws_chiles02/copy_mstransform_to_s3.py $@
