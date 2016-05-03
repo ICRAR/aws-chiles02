afc9cb32684e27ade2ea99bb9e815afc1656e7ed
KevinVinsen <kvn1961@gmail.com>
2016-05-03 13:47:25 +0800

    Added the new error listener

--- pipeline/user_data/island_manager_start_up.bash
+++ pipeline/user_data/island_manager_start_up.bash
@@ -7,7 +7,7 @@ df -h
 cd /home/ec2-user
 runuser -l ec2-user -c 'cd /home/ec2-user/dfms && git pull'
 runuser -l ec2-user -c 'cd /home/ec2-user/dfms && source /home/ec2-user/virtualenv/dfms/bin/activate && python setup.py install'
-runuser -l ec2-user -c 'cd /home/ec2-user/dfms && source /home/ec2-user/virtualenv/dfms/bin/activate && pip install /home/ec2-user/aws-chiles02/pipeline/pip/requirements.txt'
+runuser -l ec2-user -c 'cd /home/ec2-user/dfms && source /home/ec2-user/virtualenv/dfms/bin/activate && pip install --upgrade -r /home/ec2-user/aws-chiles02/pipeline/pip/requirements.txt'
 runuser -l ec2-user -c 'cd /home/ec2-user && git clone https://github.com/ICRAR/aws-chiles02.git'
 
 cat /home/ec2-user/.ssh/id_dfms.pub >> /home/ec2-user/.ssh/authorized_keys
