30c5ef51eec48145ba278ae62c82b8c2b00ab50d
KevinVinsen <kvn1961@gmail.com>
2016-12-22 13:22:23 +0800

    Added a test of docker in the start up

--- pipeline/user_data/island_manager_start_up.bash
+++ pipeline/user_data/island_manager_start_up.bash
@@ -9,6 +9,7 @@ runuser -l ec2-user -c 'cd /home/ec2-user/dfms && git pull'
 runuser -l ec2-user -c 'cd /home/ec2-user/dfms && source /home/ec2-user/virtualenv/dfms/bin/activate && python setup.py install'
 runuser -l ec2-user -c 'cd /home/ec2-user && git clone https://github.com/ICRAR/aws-chiles02.git'
 runuser -l ec2-user -c 'cd /home/ec2-user/dfms && source /home/ec2-user/virtualenv/dfms/bin/activate && pip install --upgrade -r /home/ec2-user/aws-chiles02/pipeline/pip/requirements.txt'
+runuser -l ec2-user -c 'cd /home/ec2-user/dfms && source /home/ec2-user/virtualenv/aws-chiles02/bin/activate && pip install --upgrade -r /home/ec2-user/aws-chiles02/pipeline/pip/requirements.txt'
 
 cat /home/ec2-user/.ssh/id_dfms.pub >> /home/ec2-user/.ssh/authorized_keys
 
