f0c5c889d999ef0c9f3758dae0ad6298c7be4841
KevinVinsen <kvn1961@gmail.com>
2017-01-06 10:09:33 +0800

    Allow clean to skip the QA

--- pipeline/casa_code/clean.py
+++ pipeline/casa_code/clean.py
@@ -122,61 +122,61 @@ def do_clean(cube_dir, min_freq, max_freq, iterations, arcsec, w_projection_plan
         pl.savefig(outfile + '.image.slice.svg')
         pl.clf()
 
-    # IA used to make profiles.
-    # Source near centre profile
-    xpos = 1992.0 / 4096 * smry['shape'][0]
-    ypos = 2218.0 / 4096 * smry['shape'][1]
-    box = rg.box([xpos - 2, ypos - 2], [xpos + 2, ypos + 2])
-    slce = ia.getprofile(region=box, unit='MHz', function='mean', axis=3)
-    fo = open(outfile + '.image.onsource_centre.txt', 'w')
-    for n in range(0, len(slce['coords'])):
-        print>> fo, slce['coords'][n], slce['values'][n]
-    fo.close()
-    pl.plot(slce['coords'], slce['values'] * 1e3)
-    pl.xlabel('Frequency (MHz)')
-    pl.ylabel('Amplitude (mJy)')
-    pl.title('Slice central source ' + outfile)
-    pl.savefig(outfile + '.image.onsource_centre.svg')
-    pl.clf()
-    # Source near edge profile
-    xpos = 2972.0 / 4096 * smry['shape'][0]
-    ypos = 155.0 / 4096 * smry['shape'][1]
-    box = rg.box([xpos - 2, ypos - 2], [xpos + 2, ypos + 2])
-    slce = ia.getprofile(region=box, unit='MHz', function='mean', axis=3)
-    fo = open(outfile + '.image.onsource_south.txt', 'w')
-    for n in range(0, len(slce['coords'])):
-        print>> fo, slce['coords'][n], slce['values'][n]
-    fo.close()
-    pl.plot(slce['coords'], slce['values'] * 1e3)
-    pl.xlabel('Frequency (MHz)')
-    pl.ylabel('Amplitude (mJy)')
-    pl.title('Slice central source ' + outfile)
-    pl.savefig(outfile + '.image.onsource_south.svg')
-    pl.clf()
-    # Boresight profile
-    box = rg.box([image_size / 2 - 2, image_size / 2 - 2], [image_size / 2 + 2, image_size / 2 + 2])
-    slce = ia.getprofile(region=box, unit='MHz', function='mean', axis=3)
-    fo = open(outfile + '.image.boresight.txt', 'w')
-    for n in range(0, len(slce['coords'])):
-        print>> fo, slce['coords'][n], slce['values'][n]
-    fo.close()
-    pl.plot(slce['coords'], slce['values'] * 1e3)
-    pl.xlabel('Frequency (MHz)')
-    pl.ylabel('Amplitude (mJy)')
-    pl.title('Slice central source ' + outfile)
-    pl.savefig(outfile + '.image.boresight.svg')
-    # RMS Stats
-    sts=ia.statistics(axes=[0,1],verbose=F)
-    fo = open(outfile + '.image.rms.txt', 'w')
-    for n in range(0, len(sts['rms'])):
-        print>> fo, slce['coords'][n], sts['rms'][n]
-    fo.close()
-    pl.plot(slce['coords'], sts['rms'] * 1e3)
-    pl.xlabel('Frequency (MHz)')
-    pl.ylabel('RMS (mJy)')
-    pl.title('RMS for ' + outfile)
-    pl.savefig(outfile + '.image.rms.svg')
-    ia.close()
+        # IA used to make profiles.
+        # Source near centre profile
+        xpos = 1992.0 / 4096 * smry['shape'][0]
+        ypos = 2218.0 / 4096 * smry['shape'][1]
+        box = rg.box([xpos - 2, ypos - 2], [xpos + 2, ypos + 2])
+        slce = ia.getprofile(region=box, unit='MHz', function='mean', axis=3)
+        fo = open(outfile + '.image.onsource_centre.txt', 'w')
+        for n in range(0, len(slce['coords'])):
+            print>> fo, slce['coords'][n], slce['values'][n]
+        fo.close()
+        pl.plot(slce['coords'], slce['values'] * 1e3)
+        pl.xlabel('Frequency (MHz)')
+        pl.ylabel('Amplitude (mJy)')
+        pl.title('Slice central source ' + outfile)
+        pl.savefig(outfile + '.image.onsource_centre.svg')
+        pl.clf()
+        # Source near edge profile
+        xpos = 2972.0 / 4096 * smry['shape'][0]
+        ypos = 155.0 / 4096 * smry['shape'][1]
+        box = rg.box([xpos - 2, ypos - 2], [xpos + 2, ypos + 2])
+        slce = ia.getprofile(region=box, unit='MHz', function='mean', axis=3)
+        fo = open(outfile + '.image.onsource_south.txt', 'w')
+        for n in range(0, len(slce['coords'])):
+            print>> fo, slce['coords'][n], slce['values'][n]
+        fo.close()
+        pl.plot(slce['coords'], slce['values'] * 1e3)
+        pl.xlabel('Frequency (MHz)')
+        pl.ylabel('Amplitude (mJy)')
+        pl.title('Slice central source ' + outfile)
+        pl.savefig(outfile + '.image.onsource_south.svg')
+        pl.clf()
+        # Boresight profile
+        box = rg.box([image_size / 2 - 2, image_size / 2 - 2], [image_size / 2 + 2, image_size / 2 + 2])
+        slce = ia.getprofile(region=box, unit='MHz', function='mean', axis=3)
+        fo = open(outfile + '.image.boresight.txt', 'w')
+        for n in range(0, len(slce['coords'])):
+            print>> fo, slce['coords'][n], slce['values'][n]
+        fo.close()
+        pl.plot(slce['coords'], slce['values'] * 1e3)
+        pl.xlabel('Frequency (MHz)')
+        pl.ylabel('Amplitude (mJy)')
+        pl.title('Slice central source ' + outfile)
+        pl.savefig(outfile + '.image.boresight.svg')
+        # RMS Stats
+        sts=ia.statistics(axes=[0,1],verbose=F)
+        fo = open(outfile + '.image.rms.txt', 'w')
+        for n in range(0, len(sts['rms'])):
+            print>> fo, slce['coords'][n], sts['rms'][n]
+        fo.close()
+        pl.plot(slce['coords'], sts['rms'] * 1e3)
+        pl.xlabel('Frequency (MHz)')
+        pl.ylabel('RMS (mJy)')
+        pl.title('RMS for ' + outfile)
+        pl.savefig(outfile + '.image.rms.svg')
+        ia.close()
 
     exportfits(imagename='{0}.image'.format(outfile), fitsimage='{0}.fits'.format(outfile))
 
