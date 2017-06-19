72ac7f046311c5ef09196a3bb5ce2b1a931b5c3d
KevinVinsen <kvn1961@gmail.com>
2017-06-19 15:15:52 +0800

    Fixing relative imports

--- magnus/slurm/move_data_slurm.sh
+++ magnus/slurm/move_data_slurm.sh
@@ -10,5 +10,6 @@
 #SBATCH --ntasks=1
 #SBATCH --export=NONE
 
+source /group/pawsey0216/kvinsen/anaconda2/envs/aws-chiles02/bin/activate aws-chiles02
 cd /group/pawsey0216/kvinsen/aws-chiles02/magnus/python_src
 cat list_keys.txt | xargs -I KEY --max-procs=8 bash -c python move_data.py 13b-266 KEY /group/pawsey0216/kvinsen/chiles_data
