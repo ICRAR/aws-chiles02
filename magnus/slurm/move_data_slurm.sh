c88e491ad0117863272c5a54febea7c4eb75392a
KevinVinsen <kvn1961@gmail.com>
2017-06-19 15:21:47 +0800

    Fixing the script

--- magnus/slurm/move_data_slurm.sh
+++ magnus/slurm/move_data_slurm.sh
@@ -11,5 +11,6 @@
 #SBATCH --export=NONE
 
 source /group/pawsey0216/kvinsen/anaconda2/envs/aws-chiles02/bin/activate aws-chiles02
+python --version
 cd /group/pawsey0216/kvinsen/aws-chiles02/magnus/python_src
-cat list_keys.txt | xargs -I KEY --max-procs=8 bash -c python move_data.py 13b-266 KEY /group/pawsey0216/kvinsen/chiles_data
+cat list_keys.txt | xargs -I KEY --max-procs=8 bash -c 'python move_data.py 13b-266 KEY /group/pawsey0216/kvinsen/chiles_data'
