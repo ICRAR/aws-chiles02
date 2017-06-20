9de18ef3054eefee35155965a686d0792940b83c
KevinVinsen <kvn1961@gmail.com>
2017-06-20 16:19:49 +0800

    Reduce to 8 versions on each node

--- machine_learning/slurm/generate_statistics.sh
+++ machine_learning/slurm/generate_statistics.sh
@@ -2,19 +2,22 @@
 
 # We are one of -N <NUMBER> running on a node as part of an array job
 # Show the environment variables
-echo "ALPS_APP_PE             = $ALPS_APP_PE"
-echo "SLURM_SUBMIT_DIR        = $SLURM_SUBMIT_DIR"
-echo "SLURM_JOB_NAME          = $SLURM_JOB_NAME"
-echo "SLURM_JOB_ID            = $SLURM_JOB_ID"
-echo "SLURM_JOB_NODELIST      = $SLURM_JOB_NODELIST"
-echo "SLURM_NTASKS            = $SLURM_NTASKS"
-echo "SLURM_JOB_CPUS_PER_NODE = $SLURM_JOB_CPUS_PER_NODE"
-echo "SLURM_JOB_NUM_NODES     = $SLURM_JOB_NUM_NODES"
-echo "SLURM_ARRAY_TASK_ID     = $SLURM_ARRAY_TASK_ID"
-echo "SLURM_ARRAY_JOB_ID      = $SLURM_ARRAY_JOB_ID"
-
 TASK_ID=`printf %d $((SLURM_ARRAY_TASK_ID * 8 + ALPS_APP_PE))`
-echo "TASK_ID                 = $TASK_ID"
+IP_ADDRESS=`hostname --ip-address`
+echo "ALPS_APP_PE             = $ALPS_APP_PE
+SLURM_SUBMIT_DIR        = $SLURM_SUBMIT_DIR
+SLURM_JOB_NAME          = $SLURM_JOB_NAME
+SLURM_JOB_ID            = $SLURM_JOB_ID
+SLURM_JOB_NODELIST      = $SLURM_JOB_NODELIST
+SLURM_NTASKS            = $SLURM_NTASKS
+SLURM_JOB_CPUS_PER_NODE = $SLURM_JOB_CPUS_PER_NODE
+SLURM_JOB_NUM_NODES     = $SLURM_JOB_NUM_NODES
+SLURM_ARRAY_TASK_ID     = $SLURM_ARRAY_TASK_ID
+SLURM_ARRAY_JOB_ID      = $SLURM_ARRAY_JOB_ID
+
+IP_ADDRESS              = $IP_ADDRESS
+TASK_ID                 = $TASK_ID
+########################################"
 
 cd /group/pawsey0216/kvinsen/aws-chiles02/machine_learning/casa_code
 export PYTHONPATH=/group/pawsey0216/kvinsen/aws-chiles02/machine_learning
--- machine_learning/slurm/generate_statistics_slurm.sh
+++ machine_learning/slurm/generate_statistics_slurm.sh
@@ -4,10 +4,8 @@
 #SBATCH --job-name=generate_statistics
 #SBATCH --account=pawsey0216
 #SBATCH --time=06:00:00
-#SBATCH --ntasks=24
-#SBATCH --ntasks-per-node=24
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --array=0-127
 
-# with the node=2 this will run 16 versions of test_array across two nodes (8 on each)
-aprun -n 16 -N 16 $SLURM_SUBMIT_DIR/generate_statistics.sh
+# with the node=1 this will run 8 versions of generate_statistics across the node
+aprun -n 8 -N 8 $SLURM_SUBMIT_DIR/generate_statistics.sh
