d7ea3b4d8d319b623464d4cd457231fe6f8cca32
KevinVinsen <kvn1961@gmail.com>
2017-06-20 14:12:07 +0800

    Test aprun parameters

--- magnus/slurm/test_array_slurm.sh
+++ magnus/slurm/test_array_slurm.sh
@@ -4,10 +4,8 @@
 #SBATCH --job-name=test_array
 #SBATCH --account=pawsey0216
 #SBATCH --time=00:00:10
-# BATCH --ntasks=24
-# BATCH --ntasks-per-node=24
 #SBATCH --nodes=1
 #SBATCH --array=0-4
 
 # with the node=1 this will run 24 versions of generate_statistics on the same one
-aprun -n 24 -N 24 $SLURM_SUBMIT_DIR/test_array.sh
+aprun -n 12 -N 12 $SLURM_SUBMIT_DIR/test_array.sh
