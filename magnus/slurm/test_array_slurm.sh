d6be6668fe3f13ae726a4d75e9660bc3d5c1abee
KevinVinsen <kvn1961@gmail.com>
2017-06-20 15:36:14 +0800

    Reduce to 6 versions of casa on each node

--- magnus/slurm/test_array_slurm.sh
+++ magnus/slurm/test_array_slurm.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name=test_array
 #SBATCH --account=pawsey0216
 #SBATCH --time=00:00:10
-#SBATCH --nodes=2
+#SBATCH --nodes=4
 #SBATCH --array=0-4
 
-# with the node=2 this will run 16 versions of test_array across two nodes (8 on each)
-aprun -n 16 -N 16 $SLURM_SUBMIT_DIR/test_array.sh
+# with the node=4 this will run 24 versions of test_array across four nodes (6 on each)
+aprun -n 6 -N 6 $SLURM_SUBMIT_DIR/test_array.sh
