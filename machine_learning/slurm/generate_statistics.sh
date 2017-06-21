d07b21c6a6a36181d903680e80c741f2f1d12a14
KevinVinsen <kvn1961@gmail.com>
2017-06-21 13:58:14 +0800

    Turn off log files

--- machine_learning/slurm/generate_statistics.sh
+++ machine_learning/slurm/generate_statistics.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Space out the starting of the python
+SLEEP_TIME=`printf %d $((30 * ALPS_APP_PE + 10))`
+sleep ${SLEEP_TIME}
+
 # We are one of -N <NUMBER> running on a node as part of an array job
 # Show the environment variables
 TASK_ID=`printf %d $((SLURM_ARRAY_TASK_ID * 4 + ALPS_APP_PE))`
-SLEEP_TIME=`printf %d $((30 * ALPS_APP_PE + 10))`
 IP_ADDRESS=`hostname --ip-address`
 echo "ALPS_APP_PE             = $ALPS_APP_PE
 SLURM_SUBMIT_DIR        = $SLURM_SUBMIT_DIR
@@ -21,11 +24,7 @@ TASK_ID                 = $TASK_ID
 SLEEP_TIME              = $SLEEP_TIME
 ########################################"
 
-CASA_LOG=$SLURM_SUBMIT_DIR/casa_${TASK_ID}_${SLURM_ARRAY_TASK_ID}_${ALPS_APP_PE}.log
 cd /group/pawsey0216/kvinsen/aws-chiles02/machine_learning/casa_code
 export PYTHONPATH=/group/pawsey0216/kvinsen/aws-chiles02/machine_learning
 
-# Space out the starting of the python
-sleep ${SLEEP_TIME}
-
-/group/pawsey0216/kvinsen/casa-release-4.7.2-el6/bin/casa --logfile $CASA_LOG --nologger --log2term --nogui -c generate_statistics.py 13b-266 uvsub_deep_2017_05_25 ${TASK_ID} --magnus --settings_file_name /group/pawsey0216/kvinsen/aws-chiles02/machine_learning/casa_code/scan.settings
+/group/pawsey0216/kvinsen/casa-release-4.7.2-el6/bin/casa --nologfile --nologger --log2term --nogui -c generate_statistics.py 13b-266 uvsub_deep_2017_05_25 ${TASK_ID} --magnus --settings_file_name /group/pawsey0216/kvinsen/aws-chiles02/machine_learning/casa_code/scan.settings
