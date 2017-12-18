7c847610e1f975a64150284341c8609d4f1e0442
KevinVinsen <kvn1961@gmail.com>
2017-12-18 14:03:37 +0800

    Move to optical

--- pipeline/casa_code/mstransform.py
+++ pipeline/casa_code/mstransform.py
@@ -63,9 +63,9 @@ def do_mstransform(infile, outdir, min_freq, max_freq, list_obs_json):
                 restfreq='1420.405752MHz',
                 mode='frequency',
                 nchan=no_chan,
-                outframe='topo', # TODO:
+                outframe='lsrk',
                 interpolation='linear',
-                veltype='radio',
+                veltype='optical',
                 start='{}MHz'.format(min_freq),
                 width='{}kHz'.format(width_freq),
                 spw=spw_range,
