449ddd8e233b3cb88f3fa1c02593471e16a1179e
KevinVinsen <kvn1961@gmail.com>
2017-12-19 15:21:54 +0800

    ADded a hack to make the channels a bit wider

--- pipeline/casa_code/mstransform.py
+++ pipeline/casa_code/mstransform.py
@@ -49,6 +49,11 @@ def do_mstransform(infile, outdir, min_freq, max_freq, list_obs_json):
     step_freq = max_freq - min_freq
     no_chan = int(step_freq * 1000.0 / width_freq)  # MHz/kHz!!
 
+    # HACK
+    no_chan = 60
+    width_freq = 66.666666667
+    # HACK
+
     LOG.info('spw_range: {}, no_chan: {}, width_freq: {}'.format(spw_range, no_chan, width_freq))
     if spw_range.startswith('-1') or spw_range.endswith('-1'):
         LOG.info('The spw_range is {0} which is outside the spectral window '.format(spw_range))
