a14ae79b630ad310e2b9c8235e4eea31b6df47b3
Kevin Vinsen <kvn1961@gmail.com>
2019-05-15 13:01:35 +0800

    Adding storage tags

---
+++ pipeline/aws_chiles02/add_tags_to_s3.py
@@ -0,0 +1,82 @@
+#
+#    Copyright (c) UWA, The University of Western Australia
+#    M468/35 Stirling Hwy
+#    Perth WA 6009
+#    Australia
+#
+#    All rights reserved 2019
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+"""
+
+"""
+import argparse
+import logging
+
+import boto3
+
+from aws_chiles02.common import set_logging_level
+
+LOGGER = logging.getLogger(__name__)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser('S3 objects to move')
+    parser.add_argument('bucket', help='the bucket to access')
+    parser.add_argument('prefixes_to_copy', nargs=argparse.ONE_OR_MORE)
+    parser.add_argument('-d', '--dry_run', action='store_true', help='a dry run')
+    return parser.parse_args()
+
+
+def main():
+    set_logging_level(0)
+    arguments = parse_arguments()
+
+    session = boto3.Session(profile_name='aws-chiles02')
+    s3 = session.resource('s3', use_ssl=False)
+    bucket = s3.Bucket(arguments.bucket)
+
+    keys = []
+    for prefix in arguments.prefixes_to_copy:
+        for key in bucket.objects.filter(Prefix=prefix):
+            if not key.key.endswith('/'):
+                keys.append(key.key)
+
+    for key in keys:
+        if arguments.dry_run:
+            LOGGER.info('Dry run: {}'.format(key))
+        else:
+            LOGGER.info('Staring copy: {}'.format(key))
+            copy_source = {
+                'Bucket': arguments.bucket,
+                'Key': key
+            }
+
+            s3.copy(
+                copy_source,
+                arguments.bucket,
+                key,
+                ExtraArgs={
+                    'StorageClass': 'ONE_ZONE_IA',
+                    'MetadataDirective': 'COPY'
+                }
+            )
+            LOGGER.info('Copy complete: {}'.format(key))
+
+
+if __name__ == "__main__":
+    main()
