9719e94cf1a5501617328787e2c64fafa2b525f9
Kevin Vinsen <kvn1961@gmail.com>
2019-07-02 10:33:14 +0800

    Modify the user-data

--- pipeline/user_data/island_manager_start_up.bash
+++ pipeline/user_data/island_manager_start_up.bash
@@ -24,16 +24,16 @@ cat /home/ec2-user/.ssh/id_daliuge.pub >> /home/ec2-user/.ssh/authorized_keys
 
 # Do we need a node manager running
 % if need_node_manager:
-runuser -l ec2-user -c 'cd /home/ec2-user/daliuge && source /home/ec2-user/virtualenv/daliuge/bin/activate && dlg nm --daemon -${log_level} --dlg-path=/home/ec2-user/aws-chiles02/pipeline -H 0.0.0.0 --log-dir /tmp --error-listener=aws_chiles02.error_handling.ErrorListener --max-request-size ${max_request_size}'
+runuser -l ec2-user -c 'cd /home/ec2-user && source /home/ec2-user/virtualenv/daliuge/bin/activate && dlg nm --daemon -${log_level} --dlg-path=/home/ec2-user/aws-chiles02/pipeline -H 0.0.0.0 --log-dir /tmp --error-listener=aws_chiles02.error_handling.ErrorListener --max-request-size ${max_request_size}'
 
 # Get my public IP address
 METADATA_URL_BASE="http://169.254.169.254/latest"
 export dim_ip=$(curl --silent $METADATA_URL_BASE/meta-data/public-ipv4)
-runuser -l ec2-user -c "cd /home/ec2-user/daliuge && source /home/ec2-user/virtualenv/daliuge/bin/activate && dlg dim --daemon -${log_level} -H 0.0.0.0 --ssh-pkey-path ~/.ssh/id_daliuge --nodes ${hosts},$dim_ip --log-dir /tmp --max-request-size ${max_request_size}"
+runuser -l ec2-user -c "cd /home/ec2-user && source /home/ec2-user/virtualenv/daliuge/bin/activate && dlg dim --daemon -${log_level} -H 0.0.0.0 --ssh-pkey-path ~/.ssh/id_daliuge --nodes ${hosts},$dim_ip --log-dir /tmp --max-request-size ${max_request_size}"
 % endif
 
 % if not need_node_manager:
-runuser -l ec2-user -c 'cd /home/ec2-user/daliuge && source /home/ec2-user/virtualenv/daliuge/bin/activate && dlg dim --daemon -${log_level} -H 0.0.0.0 --ssh-pkey-path ~/.ssh/id_daliuge --nodes ${hosts} --log-dir /tmp --max-request-size ${max_request_size}'
+runuser -l ec2-user -c 'cd /home/ec2-user && source /home/ec2-user/virtualenv/daliuge/bin/activate && dlg dim --daemon -${log_level} -H 0.0.0.0 --ssh-pkey-path ~/.ssh/id_daliuge --nodes ${hosts} --log-dir /tmp --max-request-size ${max_request_size}'
 % endif
 
 sleep 10
